e2a4524a879f2e77fae7ba10369bc2c3182642e8
Zahit Egemen İm <zegemenim@gmail.com>
2023-03-30 08:23:48 +0300

    created header and footer

---
+++ components/footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+
+import styles from "./styles.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+
+            <Link href="" target="_blank">
+                zegemenim
+            </Link>
+        </footer>
+    )
+}
+
+export default Footer;
